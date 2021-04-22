dd480a47ea2900f90bd1d48fa3fede6571710780
jcelerier <jeanmichael.celerier+github@gmail.com>
2021-04-22 16:33:22 +0000

    Deploying to gh-pages from @ ossia/score@6626462e186b6f6a2c006f65a037f94c6d38974e 🚀

--- struct_gfx_1_1_settings_1_1_graphics_apis.js
+++ struct_gfx_1_1_settings_1_1_graphics_apis.js
@@ -1,6 +1,6 @@
 var struct_gfx_1_1_settings_1_1_graphics_apis =
 [
-    [ "operator QStringList", "struct_gfx_1_1_settings_1_1_graphics_apis.html#a1c4a8254e804339e2f4c5e6f5ab03588", null ],
+    [ "operator QStringList", "struct_gfx_1_1_settings_1_1_graphics_apis.html#a77fbcd528c1674ba951613a76b8badf1", null ],
     [ "D3D11", "struct_gfx_1_1_settings_1_1_graphics_apis.html#a298d911444a06a5b6c6893b35e26a611", null ],
     [ "Metal", "struct_gfx_1_1_settings_1_1_graphics_apis.html#a7c78e9240a968fb5e3e9a53ddaad9bfa", null ],
     [ "OpenGL", "struct_gfx_1_1_settings_1_1_graphics_apis.html#a8406c9f323bb052091fb7dfbd413ca65", null ],
