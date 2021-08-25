3c1c435b4e32ac684c3c88e1da0dc9efec3c93fb
jcelerier <jeanmichael.celerier+github@gmail.com>
2021-08-25 21:43:59 +0000

    Deploying to gh-pages from @ ossia/score@31382272923472781217662f56729c92014a6749 🚀

--- struct_nodes_1_1_arpeggiator_1_1_node_1_1_state.js
+++ struct_nodes_1_1_arpeggiator_1_1_node_1_1_state.js
@@ -4,9 +4,9 @@ var struct_nodes_1_1_arpeggiator_1_1_node_1_1_state =
     [ "octavize", "struct_nodes_1_1_arpeggiator_1_1_node_1_1_state.html#a0453ae6a552ff8339c687c4862cf9eac", null ],
     [ "update", "struct_nodes_1_1_arpeggiator_1_1_node_1_1_state.html#a1640943ea4847e6de8c5d5a4c108b026", null ],
     [ "arpeggio", "struct_nodes_1_1_arpeggiator_1_1_node_1_1_state.html#a19d482122a078b378b0bb674c7f0e98b", null ],
+    [ "in_flight", "struct_nodes_1_1_arpeggiator_1_1_node_1_1_state.html#a1ec962371d6fef7b0b1af6c54d2c0eee", null ],
     [ "index", "struct_nodes_1_1_arpeggiator_1_1_node_1_1_state.html#a444c9f7c86b8ae4c5fcae8c96b5f20f8", null ],
     [ "notes", "struct_nodes_1_1_arpeggiator_1_1_node_1_1_state.html#a102c7bcc442b2f748b79fdcc39e6a4f9", null ],
     [ "previous_arpeggio", "struct_nodes_1_1_arpeggiator_1_1_node_1_1_state.html#ad4751baf7e954812d57189758e2054ce", null ],
-    [ "previous_chord", "struct_nodes_1_1_arpeggiator_1_1_node_1_1_state.html#ae72e32bab49ce5aaaa490d3831a499e0", null ],
     [ "previous_octave", "struct_nodes_1_1_arpeggiator_1_1_node_1_1_state.html#a79306c30421b501a4b3e3777750c3fe7", null ]
 ];
