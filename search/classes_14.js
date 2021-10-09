8a29651ca2498319b506b19b405aec29920da212
jcelerier <jeanmichael.celerier+github@gmail.com>
2021-10-09 08:39:05 +0000

    Deploying to gh-pages from @ ossia/score@4b6950c5365c74f3e7a0f390e9924bd5791c32e1 🚀

--- search/classes_14.js
+++ search/classes_14.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['ubo_5126',['UBO',['../structscore_1_1gfx_1_1_text_node_1_1_u_b_o.html',1,'score::gfx::TextNode::UBO'],['../structscore_1_1gfx_1_1_texgen_node_1_1ubo.html',1,'score::gfx::TexgenNode::ubo'],['../structscore_1_1gfx_1_1_images_node_1_1_u_b_o.html',1,'score::gfx::ImagesNode::UBO']]],
+  ['ubo_5126',['UBO',['../structscore_1_1gfx_1_1_text_node_1_1_u_b_o.html',1,'score::gfx::TextNode::UBO'],['../structscore_1_1gfx_1_1_images_node_1_1_u_b_o.html',1,'score::gfx::ImagesNode::UBO'],['../structscore_1_1gfx_1_1_texgen_node_1_1ubo.html',1,'score::gfx::TexgenNode::ubo']]],
   ['udpwidget_5127',['UDPWidget',['../class_protocols_1_1_u_d_p_widget.html',1,'Protocols']]],
   ['ui_5128',['UI',['../struct_faust_1_1_u_i.html',1,'Faust']]],
   ['uiwrapper_5129',['UiWrapper',['../struct_pd_1_1_ui_wrapper.html',1,'Pd']]],
