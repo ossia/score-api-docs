f7633dc201f99a18638601890c5caa55d8535468
jcelerier <jeanmichael.celerier+github@gmail.com>
2022-02-25 13:38:16 +0000

    Deploying to gh-pages from @ ossia/score@2aae6775e7c8004e4865e28556f17131ea587d5b 🚀

--- search/all_17.js
+++ search/all_17.js
@@ -18,7 +18,7 @@ var searchData=
   ['wiimoteprotocolfactory_2835',['WiimoteProtocolFactory',['../class_protocols_1_1_wiimote_protocol_factory.html',1,'Protocols']]],
   ['wiimoteprotocolsettingswidget_2836',['WiimoteProtocolSettingsWidget',['../class_protocols_1_1_wiimote_protocol_settings_widget.html',1,'Protocols']]],
   ['wiimotespecificsettings_2837',['WiimoteSpecificSettings',['../struct_protocols_1_1_wiimote_specific_settings.html',1,'Protocols']]],
-  ['window_2838',['Window',['../classscore_1_1gfx_1_1_window.html',1,'score::gfx::Window'],['../class_l_v2_1_1_window.html',1,'LV2::Window'],['../classvst_1_1_window.html',1,'vst::Window'],['../classvst3_1_1_window.html',1,'vst3::Window'],['../class_y_s_f_x_1_1_window.html',1,'YSFX::Window']]],
+  ['window_2838',['Window',['../class_l_v2_1_1_window.html',1,'LV2::Window'],['../classvst_1_1_window.html',1,'vst::Window'],['../classscore_1_1gfx_1_1_window.html',1,'score::gfx::Window'],['../class_y_s_f_x_1_1_window.html',1,'YSFX::Window'],['../classvst3_1_1_window.html',1,'vst3::Window']]],
   ['window_5fdevice_2839',['window_device',['../class_gfx_1_1window__device.html',1,'Gfx']]],
   ['windowcontainer_2840',['WindowContainer',['../structvst3_1_1_window_container.html',1,'vst3']]],
   ['windowdevice_2841',['WindowDevice',['../class_gfx_1_1_window_device.html',1,'Gfx']]],
