edf5f0e9443c3966cd273ef3b9c2d78923686f7e
jcelerier <jeanmichael.celerier+github@gmail.com>
2022-04-24 08:30:31 +0000

    Deploying to gh-pages from @ ossia/score@c1a2a3d27b3487eef2e53b06c9f6209c2d855a7f 🚀

--- struct_protocols_1_1_m_i_d_i_specific_settings.js
+++ struct_protocols_1_1_m_i_d_i_specific_settings.js
@@ -8,5 +8,7 @@ var struct_protocols_1_1_m_i_d_i_specific_settings =
     [ "createWholeTree", "struct_protocols_1_1_m_i_d_i_specific_settings.html#abf02cc93a9b4d649449462339194e5a2", null ],
     [ "endpoint", "struct_protocols_1_1_m_i_d_i_specific_settings.html#a96edc66808394ecd36565c282ed78a4f", null ],
     [ "io", "struct_protocols_1_1_m_i_d_i_specific_settings.html#a45d0b66099e23983018916be08499b2f", null ],
-    [ "port", "struct_protocols_1_1_m_i_d_i_specific_settings.html#ac5393555c2e6eda810e8d69e1cd5c1c0", null ]
+    [ "name", "struct_protocols_1_1_m_i_d_i_specific_settings.html#aed1c55ef6309a262de2cb4f6aea9ad8e", null ],
+    [ "port", "struct_protocols_1_1_m_i_d_i_specific_settings.html#ac5393555c2e6eda810e8d69e1cd5c1c0", null ],
+    [ "virtualPort", "struct_protocols_1_1_m_i_d_i_specific_settings.html#ad256b7fc3acf2b9248057e95cd1cba9c", null ]
 ];
