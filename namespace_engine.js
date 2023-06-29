456215b7bd717b76f5d1717bfcb608a237c36b92
jcelerier <jeanmichael.celerier+github@gmail.com>
2023-06-29 04:36:11 +0000

    Deploying to gh-pages from @ ossia/score@867361288376094e9e6b749f3572dda69463960c 🚀

--- namespace_engine.js
+++ namespace_engine.js
@@ -12,8 +12,9 @@ var namespace_engine =
       [ "expression", "score2_o_s_s_i_a_8cpp.html#a4fd234373c53268c5a385d2b8601ce6c", null ],
       [ "floatCurve", "_curve_conversion_8hpp.html#a2740c1f7979af09ae01a0f74f0ae3685", null ],
       [ "message", "score2_o_s_s_i_a_8cpp.html#acc7170596457206e27151fbee9b714e0", null ],
-      [ "state", "score2_o_s_s_i_a_8cpp.html#a5dd34827ddba7ca27f2c0024e314655e", null ],
-      [ "state", "score2_o_s_s_i_a_8cpp.html#acd7f8e302c08e5722e3d9f2ea3b3bebc", null ],
+      [ "play_state_from_ui", "score2_o_s_s_i_a_8cpp.html#a71b8ad4d9fe2641013cac3e86bde5012", null ],
+      [ "state", "score2_o_s_s_i_a_8cpp.html#a0266b9f31c03952c21538f64b33e1876", null ],
+      [ "state", "score2_o_s_s_i_a_8cpp.html#a7d6c34cf5f04ed2489c8ce4d123e5b4d", null ],
       [ "trigger_expression", "score2_o_s_s_i_a_8cpp.html#aeca6df6d72fd8e20807aa8d97645ab23", null ]
     ] ],
     [ "ApplicationPlugin", "class_engine_1_1_application_plugin.html", "class_engine_1_1_application_plugin" ]
