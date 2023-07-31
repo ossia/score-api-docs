e83535f89739e04e09af56787052af1d2d0feffa
jcelerier <jeanmichael.celerier+github@gmail.com>
2023-07-31 00:20:08 +0000

    Deploying to gh-pages from @ ossia/score@ed9fc1932617d7ff62c477906c8723594983cd25 🚀

--- struct_json_value.js
+++ struct_json_value.js
@@ -11,9 +11,11 @@ var struct_json_value =
     [ "toByteArray", "struct_json_value.html#a2735d0690549321036090afa531330bc", null ],
     [ "toDouble", "struct_json_value.html#a28a6ac8a3ed30414da6903127d6cf96e", null ],
     [ "toInt", "struct_json_value.html#ab7eed70f315c8cc240afd7eac0515406", null ],
+    [ "toInt64", "struct_json_value.html#a5f936b1783ddc83b28e5d1c78b6d316f", null ],
     [ "toObject", "struct_json_value.html#a73254f5fcd1b0ccbfc89c667ac53df25", null ],
     [ "toStdString", "struct_json_value.html#a68a8e2cc5b332d84fe0fb67322f67e36", null ],
     [ "toString", "struct_json_value.html#a920ed694d7f86a0c6f9f31d5809479b7", null ],
+    [ "toUInt64", "struct_json_value.html#ae43d67bd8c5834277e2ded47cf6a7ade", null ],
     [ "operator<<=", "struct_json_value.html#a740e5fe23fe3fc04281021d52dd57dbb", null ],
     [ "obj", "struct_json_value.html#a68390eb3fa2e062f1593e6e7e4310744", null ]
 ];
