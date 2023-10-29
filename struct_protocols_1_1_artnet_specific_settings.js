112fb693cedd789ca4dc67635d3eeef15352d0cb
jcelerier <jeanmichael.celerier+github@gmail.com>
2023-10-29 22:18:36 +0000

    Deploying to gh-pages from @ ossia/score@70110e4fb1abd6a150d6c56d6fb0cd4bfe36270d 🚀

--- struct_protocols_1_1_artnet_specific_settings.js
+++ struct_protocols_1_1_artnet_specific_settings.js
@@ -1,6 +1,6 @@
 var struct_protocols_1_1_artnet_specific_settings =
 [
-    [ "ArtNetV2", "struct_protocols_1_1_artnet_specific_settings.html#a0221dc6bd136aff4e6d06f9e8d4436bd", null ],
+    [ "ArtNetV2", "struct_protocols_1_1_artnet_specific_settings.html#a67d647738ab5fe3f0a40fbf5efbfbc6d", null ],
     [ "fixtures", "struct_protocols_1_1_artnet_specific_settings.html#a4751dce842318274b333b0d0e52d88a0", null ],
     [ "host", "struct_protocols_1_1_artnet_specific_settings.html#af633c42f6e9ab57071377281ee0bd583", null ],
     [ "rate", "struct_protocols_1_1_artnet_specific_settings.html#a4d06303be75d3e9c621899e5135d8239", null ],
