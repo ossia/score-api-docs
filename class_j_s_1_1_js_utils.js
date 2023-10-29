6fb91a72c5c2a274481796093ed1d0f0500e451e
jcelerier <jeanmichael.celerier+github@gmail.com>
2023-10-29 20:04:06 +0000

    Deploying to gh-pages from @ ossia/score@0bb581aac90055022ce1b7db604e5be3aaf57c14 🚀

--- class_j_s_1_1_js_utils.js
+++ class_j_s_1_1_js_utils.js
@@ -1,5 +1,6 @@
 var class_j_s_1_1_js_utils =
 [
     [ "layoutTextLines", "class_j_s_1_1_js_utils.html#ae0995f1739f7bfc6231822c1cc7ecfab", null ],
-    [ "readFile", "class_j_s_1_1_js_utils.html#a4f90f50b64206551cc610a5a292b75fb", null ]
+    [ "readFile", "class_j_s_1_1_js_utils.html#a4f90f50b64206551cc610a5a292b75fb", null ],
+    [ "shell", "class_j_s_1_1_js_utils.html#a3d04103954f8c7f6ab8e3a0fda956e25", null ]
 ];
