a00a23cb936fab1a98276e04e0973d69e8163988
jcelerier <jeanmichael.celerier+github@gmail.com>
2024-12-01 18:14:27 +0000

    Deploying to gh-pages from @ ossia/score@aef6075530692a7f81e34aeab178e92d40c49c87 🚀

--- class_library_1_1_recursive_filter_proxy.js
+++ class_library_1_1_recursive_filter_proxy.js
@@ -1,5 +1,6 @@
 var class_library_1_1_recursive_filter_proxy =
 [
+    [ "columnCount", "class_library_1_1_recursive_filter_proxy.html#ab54ccebd2f597d233c70c35e652f8167", null ],
     [ "filterAcceptsRow", "class_library_1_1_recursive_filter_proxy.html#a50676aa2245702c107296060e62cd6e3", null ],
     [ "filterAcceptsRowItself", "class_library_1_1_recursive_filter_proxy.html#ac0175ff620a67a0aaf871862be8cfd5f", null ],
     [ "hasAcceptedChildren", "class_library_1_1_recursive_filter_proxy.html#a028d5c1f8b0b92cc6d1639da71f34a39", null ],
